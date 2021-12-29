6f9ea679ff89e91ec2d8b4f4b5909d2687194547
Alex <alexgouveiadearaujo@gmail.com>
2021-12-29 19:57:48 -0300

    create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,14 @@
+ import './style.css'
+ 
+ function CardGame(){
+    return/*html*/`
+    
+        <article class="card-game">
+            <img class="tag-img" src="images/tag.png" />
+        </article>
+
+    `
+
+}
+
+export default CardGame;
