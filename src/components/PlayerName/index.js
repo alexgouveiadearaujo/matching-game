02f8549e9796552dae41fb0c1e0ca4946199d058
Alex <alexgouveiadearaujo@gmail.com>
2021-12-29 22:27:36 -0300

    add csss of the PlayerName component

--- src/components/PlayerName/index.js
+++ src/components/PlayerName/index.js
@@ -1,6 +1,9 @@
+import './style.css'
+
 function PlayerName(content) {
   return /*html*/ `
-        <p>${content}</p>
+        <p class="player-name">${content}</p>
     `;
 }
+
 export default PlayerName;
