d16538f5977ce0b626a8b493fae9f29dab3c9abf
Alex <alexgouveiadearaujo@gmail.com>
2022-01-05 15:48:21 -0300

    create data

---
+++ src/objects/BoardGame/data.js
@@ -0,0 +1,28 @@
+const cards = [
+  {
+    icon: "javascript",
+    altIcon: "logo js",
+  },
+  {
+    icon: "javascript",
+    altIcon: "logo js",
+  },
+  {
+    icon: "html",
+    altIcon: "logo html",
+  },
+  {
+    icon: "html",
+    altIcon: "logo html",
+  },
+  {
+    icon: "css",
+    altIcon: "logo css",
+  },
+  {
+    icon: "css",
+    altIcon: "logo css",
+  },
+];
+
+export default cards;
